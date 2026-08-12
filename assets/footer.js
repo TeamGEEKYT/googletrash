58332b3035a5e77c0ff8130d8777c53736d6693f
TeamGEEKYT <142260289+TeamGEEKYT@users.noreply.github.com>
2026-08-12 20:35:57 +0200

    Add files via upload

--- assets/footer.js
+++ assets/footer.js
@@ -1,5 +1,6 @@
 const FOOTERHTML = `<br>
-&copy; Team Geek 2026 - Google 2026<br>
+&copy; Google Trash v26.8.12_BETA2<br>
+Team Geek 2026 - Google 2026<br>
 All Rights Reserved<br>
 Google Translate is a trademark deposed by Google Co., and is not affiliated with Team Geek.<br><br>
 <a href="tos.html">Terms of Service</a><br>
